eb5551d9f03a2f010d97aae54a72fe54a038138e
inhere <in.798@qq.com>
2018-01-29 19:30:25 +0800

    add beans config file

---
+++ config/beans/base.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'application'      => [
+        'id'          => APP_NAME,
+        'name'        => APP_NAME,
+        'errorAction' => '/error/index',
+        'useProvider' => false,
+    ],
+
+];
--- config/define.php
+++ config/define.php
@@ -1,15 +1,12 @@
 <?php
-
-use \Swoft\App;
-
 // Constants
 !defined('DS') && define('DS', DIRECTORY_SEPARATOR);
 // 系统名称
-!defined('APP_NAME') && define('APP_NAME', 'swoft');
+!defined('APP_NAME') && define('APP_NAME', 'devtool');
 // 基础根目录
 !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__));
 // cli命名空间
-!defined('COMMAND_NS') && define('COMMAND_NS', "App\Commands");
+!defined('COMMAND_NS') && define('COMMAND_NS', 'Swoft\Devtool\Command');
 
 // 注册别名
 $aliases = [
@@ -23,4 +20,4 @@ $aliases = [
     '@properties' => '@configs/properties',
     '@commands'   => '@app/Commands'
 ];
-App::setAliases($aliases);
+\Swoft\App::setAliases($aliases);
