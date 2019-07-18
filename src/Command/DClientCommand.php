7f00b12aa4ff6a73a3405f5e476a8d5a56feb551
inhere <in.798@qq.com>
2019-07-18 20:05:32 +0800

    update some for dev client command

--- src/Command/DClientCommand.php
+++ src/Command/DClientCommand.php
@@ -8,6 +8,7 @@ use Swoft\Console\Annotation\Mapping\CommandOption;
 use Swoft\Console\Helper\Show;
 use Swoft\Console\Input\Input;
 use Swoft\Console\Output\Output;
+use Swoft\Tcp\Protocol;
 use Swoole\Coroutine\Client;
 use const SWOOLE_SOCK_TCP;
 
@@ -24,23 +25,34 @@ class DClientCommand
      * @CommandMapping()
      * @CommandOption("host", short="H", desc="the tcp server host address", default="127.0.0.1", type="string")
      * @CommandOption("port", short="p", desc="the tcp server port number", default="18309", type="integer")
+     * @CommandOption("split", short="s", desc="the tcp package split type: eof, len", default="eof", type="string")
      *
      * @param Input  $input
      * @param Output $output
      */
     public function tcp(Input $input, Output $output): void
     {
+        $proto = new Protocol();
+        $sType = $input->getSameOpt(['split', 's'], 'eof');
+        if ($sType === 'length') {
+            $proto->setOpenLengthCheck(true);
+        }
+
+        $output->aList([
+            'splitPackageType' => $proto->getSplitType(),
+            'dataPackerType'   => $proto->getType(),
+            'dataPackerClass'  => $proto->getPackerClass(),
+        ], 'Client Protocol');
+
         $client = new Client(SWOOLE_SOCK_TCP);
-        $client->set([
-            'open_eof_check' => true,
-            'package_eof'    => "\r\n\r\n",
-            'package_max_length' => 1024 * 1024 * 2,
-        ]);
+        $client->set($proto->getConfig());
 
         $host = $input->getSameOpt(['host', 'H'], '127.0.0.1');
         $port = $input->getSameOpt(['port', 'p'], 18309);
+        $addr = $host . ':' . $port;
 
         CONNCET:
+        $output->colored('Begin connecting to tcp server: ' . $addr);
         if (!$ok = $client->connect((string)$host, (int)$port, 5.0)) {
             $code = $client->errCode;
             /** @noinspection PhpComposerExtensionStubsInspection */
@@ -49,11 +61,11 @@ class DClientCommand
             return;
         }
 
-        $addr = $host . ':' . $port;
-        $output->colored('Successful connect to tcp server ' . $addr, 'success');
+        $output->colored('Success connect to tcp server. now, you can send message');
+        $output->title('INTERACTIVE', ['indent' => 0]);
 
         while (true) {
-            if (!$msg = $input->read('> ')) {
+            if (!$msg = $output->read('<info>client</info>> ')) {
                 $output->liteWarning('Please input message for send');
                 continue;
             }
@@ -64,12 +76,12 @@ class DClientCommand
                 break;
             }
 
-            if (false === $client->send($msg)) {
+            // Send message $msg . $proto->getPackageEOf()
+            if (false === $client->send($proto->packBody($msg))) {
                 /** @noinspection PhpComposerExtensionStubsInspection */
                 $output->error('Send error - ' . socket_strerror($client->errCode));
 
-                $yes = $input->read('Reconnect? y/n: ');
-                if ($yes && strpos($yes, 'y') === 0) {
+                if ($this->quickConfirm($input, 'Reconnect', true)) {
                     $client->close();
                     goto CONNCET;
                 }
@@ -78,10 +90,57 @@ class DClientCommand
                 break;
             }
 
-            $res = $client->recv();
-            $output->writef('Return: %s', $res);
+            // Recv response
+            $res = $client->recv(2.0);
+            if ($res === false) {
+                /** @noinspection PhpComposerExtensionStubsInspection */
+                $output->error('Recv error - ' . socket_strerror($client->errCode));
+                continue;
+            }
+
+            if ($res === '') {
+                $output->info('Server closed connection');
+                if ($this->quickConfirm($input, 'Reconnect', true)) {
+                    $client->close();
+                    goto CONNCET;
+                }
+
+                $output->colored('GoodBye!');
+                break;
+            }
+
+            [$head, $body] = $proto->unpackData($res);
+            $output->prettyJSON($head);
+            $output->writef('<yellow>server</yellow>> %s', $body);
         }
 
         $client->close();
+    }
+
+    /**
+     * @CommandMapping("ws")
+     */
+    public function websocket(): void
+    {
+
+    }
+
+    /**
+     * @param Input  $input
+     * @param string $msg
+     * @param bool   $default
+     *
+     * @return bool
+     */
+    private function quickConfirm(Input $input, string $msg, bool $default = false): bool
+    {
+        $def = $default ? 'y' : 'n';
+        $yes = $input->read("{$msg}? y/n[$def]: ");
+
+        if ('' === $yes) {
+            $yes = $def;
+        }
+
+        return stripos($yes, 'y') === 0;
     }
 }
