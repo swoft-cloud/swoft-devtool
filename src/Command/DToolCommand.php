1576c4da20932099e4baa9578d93ce8c9145d987
inhere <in.798@qq.com>
2019-09-27 20:18:26 +0800

    update some for commands

---
+++ src/Command/DToolCommand.php
@@ -0,0 +1,104 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Devtool\Command;
+
+use Swoft\Console\Annotation\Mapping\Command;
+use Swoft\Console\Annotation\Mapping\CommandArgument;
+use Swoft\Console\Annotation\Mapping\CommandMapping;
+use Swoft\Console\Helper\Show;
+use Swoft\Console\Input\Input;
+use Swoft\Stdlib\Helper\Sys;
+
+/**
+ * There are some commands for application dev[by <cyan>devtool</cyan>]
+ * @Command("dtool", coroutine=false)
+ */
+class DToolCommand
+{
+    private $issueUrl = 'https://github.com/swoft-cloud/swoft/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc';
+
+    /**
+     * Open github issues page
+     *
+     * @CommandMapping(alias="issue")
+     *
+     * @CommandArgument("operate", mode=Command::ARG_REQUIRED, default="open",
+     *      desc="operate command for issues. allow: open, search, create"
+     * )
+     *
+     * @param Input $input
+     *
+     * @example
+     *   {fullCmd} open      Open github issues page
+     *   {fullCmd} search 'websocket'
+     *   {fullCmd} create 'bug xxx'
+     */
+    public function issues(Input $input): void
+    {
+        $opName = $input->getString('operate');
+        switch ($opName) {
+            case 'open':
+                $this->openIssuePage();
+                break;
+            case 'create':
+            case 'report':
+                $this->createIssue();
+                break;
+            case 'search':
+                $this->searchIssues();
+                break;
+        }
+    }
+
+    /**
+     * Open github issues page
+     *
+     * Mac：
+     * open 'https://swoft.org'
+     *
+     * Linux:
+     * x-www-browser 'https://swoft.org'
+     *
+     * Windows:
+     * cmd /c start https://swoft.org
+     */
+    public function openIssuePage(): void
+    {
+        Show::info('will open the github issues page on browser');
+        /*
+         */
+        if (Sys::isMac()) {
+            $cmd = 'open';
+        } elseif (Sys::isWin()) {
+            $cmd = 'cmd /c start';
+        } else {
+            $cmd = 'x-www-browser';
+        }
+
+        Sys::execute($cmd . ' ' . $this->issueUrl);
+    }
+
+    /**
+     * @return int
+     */
+    public function createIssue(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @return int
+     */
+    public function searchIssues(): int
+    {
+        return 0;
+    }
+
+    /**
+     * @CommandMapping(alias="pub", desc="publish some assets to application direactory")
+     */
+    public function publish(): void
+    {
+        Show::info('WIP');
+    }
+}
--- src/DevTool.php
+++ src/DevTool.php
@@ -2,36 +2,33 @@
 
 namespace Swoft\Devtool;
 
-use Swoft\App;
-use Swoft\Console\Helper\ConsoleUtil;
-use Swoft\Core\Coroutine;
+use Swoft;
 
 /**
  * Class DevTool
- * @package Swoft\Devtool
  */
 final class DevTool
 {
-    const VERSION = '1.0.0';
-    const ROUTE_PREFIX = '/__devtool';
+    public const VERSION      = '2.0.0';
+    public const ROUTE_PREFIX = '/__devtool';
 
     public static $table;
 
     /**
      * @param string $msg
-     * @param array $data
+     * @param array  $data
      * @param string $type
-     * @param array $opts
+     * @param array  $opts
      */
     public static function log(string $msg, array $data = [], string $type = 'debug', array $opts = [])
     {
-        if (App::$server && !App::$server->isDaemonize()) {
-            ConsoleUtil::log($msg, $data, $type, [
-                'DevTool',
-                'WorkerId' => App::getWorkerId(),
-                'CoId' => Coroutine::tid()
-            ]);
-        }
+        // if (Swoft::server() && !Swoft::server()->isDaemonize()) {
+        //     ConsoleUtil::log($msg, $data, $type, [
+        //         'DevTool',
+        //         'WorkerId' => App::getWorkerId(),
+        //         'CoId'     => Coroutine::tid()
+        //     ]);
+        // }
 
         // if ($logger = App::getLogger()) {
         //     $logger->log($type, $msg, $data);
